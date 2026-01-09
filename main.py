2b44f4038e07298c6d4698285ee357a11f5da83a
atauln <ataulnoor75@gmail.com>
2026-01-09 16:54:06 -0500

    added archidekt processing

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from pprint import pprint
 
 
 deck = getDeckById(18053512)
-pprint(vars(deck), indent=2, depth=3, sort_dicts=False)
+
+print([c.name for c in deck.categories])
+
 # for category in deck.categories:
 #     print(f"Category: {category.name}")
 #     for card in category.cards:
